1f7226a73c3b12e3e41cbd41064957f15fcd07c3
jamal bajady <jamalbajady@gmail.com>
2024-10-02 13:12:31 +0100

    search2

--- Search-bar/main.go
+++ Search-bar/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	Handle "GroupieTracker/FuncHandle"
+)
+
+func main() {
+	http.HandleFunc("/", Handle.HomeHandle)
+	http.HandleFunc("/Artist", Handle.ArtistsHandle)
+	http.HandleFunc("/Search", Handle.SearchHandle)
+	fmt.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
