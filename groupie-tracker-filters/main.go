69f52000eed6c4e7b0c1827091f912396197efc7
jamal bajady <jamalbajady@gmail.com>
2024-10-02 13:59:51 +0100

    commit fleter2

--- groupie-tracker-filters/main.go
+++ groupie-tracker-filters/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	Handle "GroupieTracker/FuncHandle"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Handle.HomeHandle)
+	mux.HandleFunc("/Artist", Handle.ArtistsHandle)
+	fmt.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", mux)
+}
