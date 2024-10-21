5c38fb2b031f1e77d23548d479d96ed3c203cd22
jamal bajady <jbajady@b1f1r1s12.zone01oujda.ma>
2024-10-21 14:18:54 +0100

    anther commit

--- groupie-tracker-visualizations/main.go
+++ groupie-tracker-visualizations/main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/static", Handle.StaticHandle)
 	http.HandleFunc("/", Handle.HomeHandle)
 	http.HandleFunc("/Artist", Handle.ArtistsHandle)
